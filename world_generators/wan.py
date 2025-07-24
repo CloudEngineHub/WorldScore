99edfb149f0531edb0f9a7df80692ee7eeb7d2a6
Haoyi Duan <haoyid@haic-hgx-1.stanford.edu>
2025-07-23 17:07:20 -0700

    update

--- world_generators/wan.py
+++ world_generators/wan.py
@@ -5,6 +5,8 @@ import os
 import sys
 import warnings
 from datetime import datetime
+from pathlib import Path
+from typing import Literal, Optional
 
 warnings.filterwarnings('ignore')
 
@@ -65,161 +67,6 @@ EXAMPLE_PROMPT = {
 }
 
 
-def _parse_args():
-    parser = argparse.ArgumentParser(
-        description="Generate a image or video from a text prompt or image using Wan"
-    )
-    parser.add_argument(
-        "--task",
-        type=str,
-        default="t2v-14B",
-        choices=list(WAN_CONFIGS.keys()),
-        help="The task to run.")
-    parser.add_argument(
-        "--size",
-        type=str,
-        default="1280*720",
-        choices=list(SIZE_CONFIGS.keys()),
-        help="The area (width*height) of the generated video. For the I2V task, the aspect ratio of the output video will follow that of the input image."
-    )
-    parser.add_argument(
-        "--frame_num",
-        type=int,
-        default=None,
-        help="How many frames to sample from a image or video. The number should be 4n+1"
-    )
-    parser.add_argument(
-        "--ckpt_dir",
-        type=str,
-        default=None,
-        help="The path to the checkpoint directory.")
-    parser.add_argument(
-        "--offload_model",
-        type=str2bool,
-        default=None,
-        help="Whether to offload the model to CPU after each model forward, reducing GPU memory usage."
-    )
-    parser.add_argument(
-        "--ulysses_size",
-        type=int,
-        default=1,
-        help="The size of the ulysses parallelism in DiT.")
-    parser.add_argument(
-        "--ring_size",
-        type=int,
-        default=1,
-        help="The size of the ring attention parallelism in DiT.")
-    parser.add_argument(
-        "--t5_fsdp",
-        action="store_true",
-        default=False,
-        help="Whether to use FSDP for T5.")
-    parser.add_argument(
-        "--t5_cpu",
-        action="store_true",
-        default=False,
-        help="Whether to place T5 model on CPU.")
-    parser.add_argument(
-        "--dit_fsdp",
-        action="store_true",
-        default=False,
-        help="Whether to use FSDP for DiT.")
-    parser.add_argument(
-        "--save_file",
-        type=str,
-        default=None,
-        help="The file to save the generated image or video to.")
-    parser.add_argument(
-        "--src_video",
-        type=str,
-        default=None,
-        help="The file of the source video. Default None.")
-    parser.add_argument(
-        "--src_mask",
-        type=str,
-        default=None,
-        help="The file of the source mask. Default None.")
-    parser.add_argument(
-        "--src_ref_images",
-        type=str,
-        default=None,
-        help="The file list of the source reference images. Separated by ','. Default None."
-    )
-    parser.add_argument(
-        "--prompt",
-        type=str,
-        default=None,
-        help="The prompt to generate the image or video from.")
-    parser.add_argument(
-        "--use_prompt_extend",
-        action="store_true",
-        default=False,
-        help="Whether to use prompt extend.")
-    parser.add_argument(
-        "--prompt_extend_method",
-        type=str,
-        default="local_qwen",
-        choices=["dashscope", "local_qwen"],
-        help="The prompt extend method to use.")
-    parser.add_argument(
-        "--prompt_extend_model",
-        type=str,
-        default=None,
-        help="The prompt extend model to use.")
-    parser.add_argument(
-        "--prompt_extend_target_lang",
-        type=str,
-        default="zh",
-        choices=["zh", "en"],
-        help="The target language of prompt extend.")
-    parser.add_argument(
-        "--base_seed",
-        type=int,
-        default=-1,
-        help="The seed to use for generating the image or video.")
-    parser.add_argument(
-        "--image",
-        type=str,
-        default=None,
-        help="[image to video] The image to generate the video from.")
-    parser.add_argument(
-        "--first_frame",
-        type=str,
-        default=None,
-        help="[first-last frame to video] The image (first frame) to generate the video from."
-    )
-    parser.add_argument(
-        "--last_frame",
-        type=str,
-        default=None,
-        help="[first-last frame to video] The image (last frame) to generate the video from."
-    )
-    parser.add_argument(
-        "--sample_solver",
-        type=str,
-        default='unipc',
-        choices=['unipc', 'dpm++'],
-        help="The solver used to sample.")
-    parser.add_argument(
-        "--sample_steps", type=int, default=None, help="The sampling steps.")
-    parser.add_argument(
-        "--sample_shift",
-        type=float,
-        default=None,
-        help="Sampling shift factor for flow matching schedulers.")
-    parser.add_argument(
-        "--sample_guide_scale",
-        type=float,
-        default=5.0,
-        help="Classifier free guidance scale.")
-
-    args = parser.parse_args()
-
-    _validate_args(args)
-
-    return args
-
-
 def _init_logging(rank):
     # logging
     if rank == 0:
@@ -231,7 +78,7 @@ def _init_logging(rank):
     else:
         logging.basicConfig(level=logging.ERROR)
 
-class Wan2_1:
+class Wan:
   def __init__(
     self,
     model_name: str,
@@ -250,16 +97,17 @@ class Wan2_1:
     base_seed: int = -1,
     sample_solver: str = "unipc",
     sample_guide_scale: float = 5.0,
-    **kwargs
+    task: str = "i2v-14B",
+    size: str = "832*480",
+    ckpt_dir: str = "./models/Wan2.1-I2V-14B-480P",
+    frames: int = 81,
+    fps: int = 16,
+    t5_cpu: bool = False,
   ):
     # Initialize your model
     self.generation_type = generation_type
     
-    task = kwargs["task"]
-    size = kwargs["size"]
-    ckpt_dir = kwargs["ckpt_dir"]
-    frame_num = kwargs["frames"]
-    fps = kwargs["fps"]
+    frame_num = frames
     base_seed = base_seed if base_seed >= 0 else random.randint(
         0, sys.maxsize)
     
@@ -321,7 +169,6 @@ class Wan2_1:
     if ulysses_size > 1:
         assert cfg.num_heads % ulysses_size == 0, f"`{cfg.num_heads=}` cannot be divided evenly by `{ulysses_size=}`."
 
-    logging.info(f"Generation job args: {args}")
     logging.info(f"Generation model config: {cfg}")
 
     if dist.is_initialized():
@@ -349,6 +196,8 @@ class Wan2_1:
     self.sample_guide_scale = sample_guide_scale
     self.base_seed = base_seed
     self.offload_model = offload_model
+    self.size = size
+    self.fps = fps
 
     
   def generate_video(
@@ -378,6 +227,3 @@ class Wan2_1:
     # - torch.Tensor of shape [N, 3, H, W] with values in [0, 1]
     return video
 
-if __name__ == "__main__":
-    args = _parse_args()
-    generate(args)
